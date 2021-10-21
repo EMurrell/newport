82172b20613353f255716a0adf75c9f31ea9d6a7
EMurrell <82235988+EMurrell@users.noreply.github.com>
2021-10-21 11:52:50 -0400

    add html lang attribute

--- components/Meta.js
+++ components/Meta.js
@@ -4,6 +4,7 @@ export default function Meta() {
   return (
     <Head>
       <title>EMurrell.com</title>
+      <html lang="en" />
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <meta
